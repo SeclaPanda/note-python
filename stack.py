c74d9d9174180f992a61e93363a07330a5eb30af
SeclaPanda <jack345698@gmail.com>
2021-05-25 00:56:42 +0300

    modify stack.py

--- stack.py
+++ stack.py
@@ -5,52 +5,52 @@ stack_3 = []
 
 i = int(input('Count of numbers for stack_1: \n')) #add numbers in first stack
 for a in range(i):
-    stack_1.append(input())
+    stack_1 += input()
 print(stack_1)
 
 i = int(input('Count of numbers for stack_2: \n')) #add numbers in second stack 
 for a in range(i):
-    stack_2.append(input())
+    stack_2 += input()
 print(stack_2)
 
 i = int(input('Count of numbers for stack_3: \n')) #add numbers in third stack
 for a in range(i):
-    stack_3.append(input())
+    stack_3 += input()
 print(stack_3)
 
 stack_all = [] #initialize big stack
 for i in range(len(stack_1)): #add numbers from firts to big
-    stack_all.append(stack_1[i])
+    stack_all += stack_1[i]
 for i in range(len(stack_2)): #add numbers from second to big 
-    stack_all.append(stack_2[i])
+    stack_all += stack_2[i]
 for i in range(len(stack_3)): #add numbers from third to big
-    stack_all.append(stack_3[i])
+    stack_all += stack_3[i]
 print(stack_all) #print big stack
 
 menu = int(input('What u gonna do? 1 - pop, 2 - index, 3 - add, 4 - change prev stack, 5 - update all stack, 6 - exit \n')) #make an menu
 while menu != 6: #stop for menu
     if menu == 1:
-        stack_all.pop() #deleting last item in stack
+        stack_all -= stack_all[-1] #deleting last item in stack
         print(stack_all)
     elif menu == 2:
         ind = int(input('Enter index: \n')) #find an stack member
         print(stack_all[ind])
     elif menu == 3: #add last item in stack
-        cou = int(input('Enter: \n'))
-        stack_all.append(cou)
+        cou = input('Enter: \n')
+        stack_all += cou
         print(stack_all)
     elif menu == 4:
         menu_ch = int(input('What u gonna do? 1 - pop, 2 - index, 3 - add\n'))
         if menu_ch == 1:
             menu_st = int(input('Choose stack: 1, 2, 3?'))
             if menu_st == 1:
-                stack_1.pop() #deleting last item in stack
+                stack_1 -= stack_1[-1] #deleting last item in stack
                 print(stack_1)
             elif menu_st == 2:
-                stack_2.pop() #deleting last item in stack
+                stack_2 -= stack_2[-1] #deleting last item in stack
                 print(stack_2)
             elif menu_st == 3:
-                stack_3.pop() #deleting last item in stack
+                stack_3 -= stack_3[-1] #deleting last item in stack
                 print(stack_3)
             else:
                 continue
@@ -71,24 +71,24 @@ while menu != 6: #stop for menu
             menu_st = int(input('Choose stack: 1, 2, 3?'))
             if menu_st == 1: #add last item in stack
                 cou = int(input('Enter: \n'))
-                stack_1.append(cou)
+                stack_1 += cou
                 print(stack_1)
             elif menu_st == 2: #add last item in stack
                 cou = int(input('Enter: \n'))
-                stack_2.append(cou)
+                stack_2 += cou
                 print(stack_2)
             elif menu_st == 3: #add last item in stack
                 cou = int(input('Enter: \n'))
-                stack_3.append(cou)
+                stack_3 += cou
                 print(stack_3)
             else:
                 continue
     elif menu == 5:
         for i in range(len(stack_1)): #add numbers from firts to big
-            stack_all.append(stack_1[i])
+            stack_all += stack_1[i]
         for i in range(len(stack_2)): #add numbers from second to big 
-            stack_all.append(stack_2[i])
+            stack_all += stack_2[i]
         for i in range(len(stack_3)): #add numbers from third to big
-            stack_all.append(stack_3[i])
+            stack_all += stack_3[i]
         print(stack_all) #print big stack
     menu = int(input('What u gonna do? 1 - pop, 2 - index, 3 - add, 4 - change prev stack, 5 - update all stack, 6 - exit \n')) #start menu again3
