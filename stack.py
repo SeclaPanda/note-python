91a13e5cf2ab6a8821bc6d9a0942138d66117d7f
Alex <61236755+SeclaPanda@users.noreply.github.com>
2021-05-25 01:17:47 +0300

    modify stack.py (again)

--- stack.py
+++ stack.py
@@ -30,7 +30,7 @@ print(stack_all) #print big stack
 menu = int(input('What u gonna do? 1 - pop, 2 - index, 3 - add, 4 - change prev stack, 5 - update all stack, 6 - exit \n')) #make an menu
 while menu != 6: #stop for menu
     if menu == 1:
-        stack_all -= stack_all[-1] #deleting last item in stack
+        stack_all.pop() #deleting last item in stack
         print(stack_all)
     elif menu == 2:
         ind = int(input('Enter index: \n')) #find an stack member
@@ -44,13 +44,13 @@ while menu != 6: #stop for menu
         if menu_ch == 1:
             menu_st = int(input('Choose stack: 1, 2, 3?'))
             if menu_st == 1:
-                stack_1 -= stack_1[-1] #deleting last item in stack
+                stack_1.pop() #deleting last item in stack
                 print(stack_1)
             elif menu_st == 2:
-                stack_2 -= stack_2[-1] #deleting last item in stack
+                stack_2.pop() #deleting last item in stack
                 print(stack_2)
             elif menu_st == 3:
-                stack_3 -= stack_3[-1] #deleting last item in stack
+                stack_3.pop() #deleting last item in stack
                 print(stack_3)
             else:
                 continue
@@ -84,6 +84,7 @@ while menu != 6: #stop for menu
             else:
                 continue
     elif menu == 5:
+        stack_all.clear()
         for i in range(len(stack_1)): #add numbers from firts to big
             stack_all += stack_1[i]
         for i in range(len(stack_2)): #add numbers from second to big 
