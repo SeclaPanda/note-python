b01be8c2102b2bcfcbcbcb0066358795bd799533
Alex <61236755+SeclaPanda@users.noreply.github.com>
2021-07-23 19:19:16 +0300

    Make new maven proj for practice

---
+++ practice/src/test/java/uis_211_bakl/AppTest.java
@@ -0,0 +1,18 @@
+package uis_211_bakl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
